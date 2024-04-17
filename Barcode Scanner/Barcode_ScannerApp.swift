1c78bf40b15db04102e0ef2304c6ae55ed3ce015
xizana <xizoooo102@gmail.com>
2024-04-17 15:41:59 +0400

    Initial Commit

---
+++ Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Lasha Khizanishvili on 17.04.24.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
