00085055c459bde711bc4f6692f66219cde61020
xizana <xizoooo102@gmail.com>
2024-04-17 15:44:35 +0400

    add barcodescannerview.

---
+++ Barcode Scanner/BarcodeScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  BarcodeScannerView.swift
+//  Barcode Scanner
+//
+//  Created by Lasha Khizanishvili on 17.04.24.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
